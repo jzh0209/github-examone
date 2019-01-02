923c06d73330e7a50d54ea16433ce14ed17386e0
jzh <1576803896@qq.com>
2019-01-02 09:14:49 +0800

    开发环境完成

---
+++ dist/js/index.js
@@ -0,0 +1,1 @@
+gulp.task("watch",function(){return gulp.src("./src/scss/*.scss",gulp.series("sass"))}),gulp.task("default",gulp.series("sass","server","watch")),gulp.task("bJs",function(){return gulp.src("./src/js/*.js").pipe(uglify()).pipe(gulp.dest("./dist/js"))});
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+
+var server = require('gulp-webserver');
+
+var sass = require('gulp-sass');
+
+var uglify = require('gulp-uglify');
+
+var clean = require('gulp-clean-css');
+
+var concat = require('gulp-concat');
+
+//编译scss
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'))
+})
+
+//起服务
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 8686,
+            livereload: true //自动刷新
+        }))
+})
+
+//监听
+gulp.task('watch', function() {
+    return gulp.src('./src/scss/*.scss', gulp.series('sass'))
+})
+
+//开发环境
+gulp.task('default', gulp.series('sass', 'server', 'watch'))
